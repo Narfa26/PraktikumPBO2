4ac5f9241147fa64e80c9209ca6fd19d532fe31d
FASEPT26 <farhanmuhtaram576@gmail.com>
2026-02-19 09:54:27 +0700

    Percobaan pertama

---
+++ Pertemuan1/Pertemuan1/src/Titik.java
@@ -0,0 +1,41 @@
+
+public class Titik {
+
+    // Atribut
+    private double absis;
+    private double ordinat;
+
+    // Konstruktor
+    public Titik(double a, double o) {
+        absis = a;
+        ordinat = o;
+    }
+
+    // Getter
+    public double getAbsis() {
+        return absis;
+    }
+
+    public double getOrdinat() {
+        return ordinat;
+    }
+
+    // Setter
+    public void setAbsis(double a) {
+        absis = a;
+    }
+
+    public void setOrdinat(double o) {
+        ordinat = o;
+    }
+
+    // Method untuk menampilkan titik
+    public void printTitik() {
+        System.out.println("(" + absis + ", " + ordinat + ")");
+    }
+
+    // Method untuk menghitung jarak ke titik (0,0)
+    public double hitungJarakKeOrigin() {
+        return Math.sqrt(absis * absis + ordinat * ordinat);
+    }
+}
